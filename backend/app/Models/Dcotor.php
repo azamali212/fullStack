6ef6ec161486b8ae78ebcbca61a20235399ce523
Azam Ali <azam_ali@Azams-MacBook-Pro.local>
2024-08-07 20:07:04 +0100

    new

---
+++ backend/app/Models/Dcotor.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Dcotor extends Model
+{
+    use HasFactory;
+}
