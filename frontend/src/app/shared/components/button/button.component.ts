21cd44eaaaf5053ccdb45ea3a40ae52a2367ccfc
Azam Ali <azam_ali@Mac-2.powerhub>
2024-11-05 00:54:22 +0000

    new

---
+++ frontend/src/app/shared/components/button/button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [ButtonComponent],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css'
+})
+export class ButtonComponent {
+
+}
